6785b152f5cacb256bfe0d978fe75bbd1c487318
Jason Chen <js910924@gmail.com>
2024-07-18 16:48:44 +0800

    chore: comment unused link temporary

--- components/Header.vue
+++ components/Header.vue
@@ -8,8 +8,8 @@
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/investment">Investment</a></li>
-          <li><a href="/about">About Us</a></li>
-          <li><a href="/contact">Contact</a></li>
+<!--          <li><a href="/about">About Us</a></li>-->
+<!--          <li><a href="/contact">Contact</a></li>-->
         </ul>
       </nav>
     </div>
