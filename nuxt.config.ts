ce74d6fa20b8cbd95f57ca3eae67edb2dd91c649
Jason Chen <js910924@gmail.com>
2024-07-18 17:41:22 +0800

    Revert "feat: apply sitemap for SEO"
    This reverts commit b385c9be4837e168354dcd439427a3c2429f151e.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-      '@nuxtjs/sitemap',
-  ],
+  // modules: [
+  //     '@nuxtjs/axios',
+  // ],
   // axios: {
   //   baseURL: 'http://localhost:3000/api', // Adjust this to your backend API
   // },
-  ssr: true,
-  sitemap: {
-    hostname: 'https://life-academy.netlify.app',
-    routes: [
-      '/',
-      '/investment',
-    ]
-  },
-  head: {
-    titleTemplate: 'Life Academy',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: 'Life Academy - Essential skills for a better life' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
