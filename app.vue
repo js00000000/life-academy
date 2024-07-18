d9794ee0a16a450adce9b4309e6cde1af5b368ec
Jason Chen <js910924@gmail.com>
2024-07-18 17:45:16 +0800

    feat: apply SEO meta

--- app.vue
+++ app.vue
@@ -4,3 +4,10 @@
     <NuxtLayout />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: `Life Academy`,
+  description: 'Life Academy - Essential skills for a better life',
+})
+</script>
