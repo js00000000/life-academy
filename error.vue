2713853f6b4ddff73d2b63dbba8775de0195a2e5
Jason Chen <js910924@gmail.com>
2024-07-18 17:11:41 +0800

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+</script>
+
+<template>
+  <div>
+    <h1 v-if="props.error!.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
